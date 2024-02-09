fb3e309f771ea9d9023b25ebc60d211c70f46ad0
Fábio Jr. Griesang <fabio.griesang@outlook.com>
2024-02-09 13:38:10 -0300

    atualizado para validar caracteres no textearea

--- app.js
+++ app.js
@@ -56,7 +56,6 @@ function criptando(){
 }
 
 //bonus: botão copiar
-
 function escondeCopiaInfo(){
     document.getElementById('mensagemCopia').style.display='none';
 }
@@ -77,166 +76,212 @@ function funcaoCopiar() {
     setTimeout(escondeCopiaInfo, 2000);
 }
 
+//função validar caracteres
+function validaCaracteres(texareaValue) { 
+    let pattern = new RegExp(/[A-Z\u00C0-\u00FF\-0-9\!@#\$%\^\&*\)\(+=._-]/gm,'');
+        //"^([a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[-+_!@#$%^&*.,?]).+$" 
+  
+    if (pattern.test(texareaValue)) 
+        return false; 
+    else
+        return true; 
+    return; 
+}
+
 //função criptografar
 function criptografar(){
 
-    //validar textearea
-    if(document.getElementById('texto').value==''){  
-        document.getElementById('texto').focus();
-        document.getElementById('descriptografarBotao').style.cursor='not-allowed';
-        document.getElementById("descriptografarBotao").disabled = true;
-        document.getElementById('texto').placeholder='Informe o texto a ser Criptografado';
-    } else {
-                
-        document.getElementById('descriptografarBotao').style.cursor='auto';
-        document.getElementById("descriptografarBotao").disabled = false;
+        //obter os valores do textarea
+        const texareaValue = document.getElementById('texto').value;
+
+        let resultadoValidado =  validaCaracteres(texareaValue);
+        alert (resultadoValidado);
         
+        if (resultadoValidado == true){
 
-        //zerar elementos inicais da tela
-        document.getElementById('apresentacao__conteudo__direita__img').style.background='#FFF';
-        document.getElementById('apresentacao__conteudo__direita__img').style.height='1px';
-        let imagemInicialGet = '';
-        imagemInicialGet.innerHTML= '';
-        tituloH4.innerHTML = '';
-        instrucaoInicialGet.innerHTML = '';
-        botaoCopiarGet.innerHTML = botaoCopiar;
-
-        //selecionar o textarea
-        converteLoweGet = document.getElementById('texto').value;
-        textoDigitado = converteLoweGet.toLocaleLowerCase();
-
-        //contar caracteres no texto
-        let quantidadeCaracteres = textoDigitado.length;
-
-        //limpar margin section direita 
-        document.getElementById('apresentacao__conteudo__direita__img').style.margin='0';
-        document.getElementById('apresentacao__conteudo__direita__info').style.margin='0';
-        document.getElementById('instrucao').style.margin='0';
-        instrucao
-        
-        //loop para realizar a criptografia
-        for(let i=0; i<quantidadeCaracteres; i++) {
+        //validar textearea
+        if(document.getElementById('texto').value==''){  
+            document.getElementById('texto').focus();
+            document.getElementById('descriptografarBotao').style.cursor='not-allowed';
+            document.getElementById("descriptografarBotao").disabled = true;
+            document.getElementById('texto').placeholder='Informe o texto a ser Criptografado';
+        } else {
+                    
+            document.getElementById('descriptografarBotao').style.cursor='auto';
+            document.getElementById("descriptografarBotao").disabled = false;
             
-            if(textoDigitado[i] == 'e') {
-                novoTexoDigitado[i] = 'enter';
-            }else if(textoDigitado[i] == 'i'){
-                novoTexoDigitado[i] = 'imes';
-            }
-            else if(textoDigitado[i] == 'a'){
-                novoTexoDigitado[i] = 'ai';
-            }
-            else if(textoDigitado[i] == 'o'){
-                novoTexoDigitado[i] = 'ober';
-            }
-            else if (textoDigitado[i] == 'u') {
-                novoTexoDigitado[i] = 'ufat';
-            }else {
-                novoTexoDigitado[i] = textoDigitado[i];
-            }
+
+            //zerar elementos inicais da tela
+            document.getElementById('apresentacao__conteudo__direita__img').style.background='#FFF';
+            document.getElementById('apresentacao__conteudo__direita__img').style.height='1px';
+            let imagemInicialGet = '';
+            imagemInicialGet.innerHTML= '';
+            tituloH4.innerHTML = '';
+            instrucaoInicialGet.innerHTML = '';
+            botaoCopiarGet.innerHTML = botaoCopiar;
+
+            //selecionar o textarea
+            converteLoweGet = document.getElementById('texto').value;
+
+            textoDigitado = converteLoweGet.toLocaleLowerCase();
+
+            //contar caracteres no texto
+            let quantidadeCaracteres = textoDigitado.length;
+
+            //limpar margin section direita 
+            document.getElementById('apresentacao__conteudo__direita__img').style.margin='0';
+            document.getElementById('apresentacao__conteudo__direita__info').style.margin='0';
+            document.getElementById('instrucao').style.margin='0';
+            instrucao
+            
+            //loop para realizar a criptografia
+            for(let i=0; i<quantidadeCaracteres; i++) {
+                
+                if(textoDigitado[i] == 'e') {
+                    novoTexoDigitado[i] = 'enter';
+                }else if(textoDigitado[i] == 'i'){
+                    novoTexoDigitado[i] = 'imes';
+                }
+                else if(textoDigitado[i] == 'a'){
+                    novoTexoDigitado[i] = 'ai';
+                }
+                else if(textoDigitado[i] == 'o'){
+                    novoTexoDigitado[i] = 'ober';
+                }
+                else if (textoDigitado[i] == 'u') {
+                    novoTexoDigitado[i] = 'ufat';
+                }else {
+                    novoTexoDigitado[i] = textoDigitado[i];
+                }
 
 
+                //exibir dados
+                let textoCriptorafado = document.getElementById('MostraCriptoDescripto');
+                let textoCriptorafadoRows = document.querySelector('textarea');
 
-            //exibir dados
-            let textoCriptorafado = document.getElementById('MostraCriptoDescripto');
-            let textoCriptorafadoRows = document.querySelector('textarea');
-
-            //ajuste do textarea lado direito
-            if (resolucaoLargura > 1100){
-                textoCriptorafado.innerHTML = '<textarea minlength="1" style="height: 655px;" maxlength="380" disabled id="resultado">'+novoTexoDigitado.join('')+'</textarea>';
-            } else if (resolucaoLargura <= 1100){
-                textoCriptorafado.innerHTML = '<textarea minlength="1" style="height: 300px;" maxlength="380" disabled id="resultado">'+novoTexoDigitado.join('')+'</textarea>';
-            } else if(resolucaoLargura <= 450){
-                textoCriptorafado.innerHTML = '<textarea minlength="1" style="height: 200px;" maxlength="380" disabled id="resultado">'+novoTexoDigitado.join('')+'</textarea>';
+                //ajuste do textarea lado direito
+                if (resolucaoLargura > 1100){
+                    textoCriptorafado.innerHTML = '<textarea minlength="1" style="height: 655px;" maxlength="380" disabled id="resultado">'+novoTexoDigitado.join('')+'</textarea>';
+                } else if (resolucaoLargura <= 1100){
+                    textoCriptorafado.innerHTML = '<textarea minlength="1" style="height: 300px;" maxlength="380" disabled id="resultado">'+novoTexoDigitado.join('')+'</textarea>';
+                } else if(resolucaoLargura <= 450){
+                    textoCriptorafado.innerHTML = '<textarea minlength="1" style="height: 200px;" maxlength="380" disabled id="resultado">'+novoTexoDigitado.join('')+'</textarea>';
+                }
+                AjustaTextarea();
             }
-            AjustaTextarea();
         }
-    } 
+
+    } else {
+        document.getElementById('texto').value='';
+        document.getElementById('texto').placeholder='letras minúsculas e sem acento!';
+        setTimeout(function(){
+            location.reload();
+        }, 2000);
+    }
+    
 }
+
 
 //função descriptografar
 function descriptografar(){
 
-    if(document.getElementById('texto').value==''){  
-        document.getElementById('texto').focus();
-        document.getElementById('texto').placeholder='Informe o texto a ser Criptografado';
+    //obter os valores do textarea
+    const texareaValue = document.getElementById('texto').value;
+
+    let resultadoValidado =  validaCaracteres(texareaValue);
+        
+    if (resultadoValidado == true){
+
+        if(document.getElementById('texto').value==''){  
+            document.getElementById('texto').focus();
+            document.getElementById('texto').placeholder='Informe o texto a ser Criptografado';
+        } else {
+
+            document.getElementById('descriptografarBotao').style.cursor='auto';
+            document.getElementById("descriptografarBotao").disabled = false;
+
+            //zerar elementos inicais da tela
+            document.getElementById('apresentacao__conteudo__direita__img').style.background='#FFF';
+            document.getElementById('apresentacao__conteudo__direita__img').style.height='1px';
+            let imagemInicialGet = '';
+            imagemInicialGet.innerHTML= '';
+            tituloH4.innerHTML = '';
+            instrucaoInicialGet.innerHTML = '';
+            botaoCopiarGet.innerHTML = botaoCopiar;
+
+            //selecionar o textarea
+            converteLoweGet = document.getElementById('texto').value;
+            frase = converteLoweGet.toLocaleLowerCase();
+
+            //limpar margin section direita 
+            document.getElementById('apresentacao__conteudo__direita__img').style.margin='0';
+            document.getElementById('apresentacao__conteudo__direita__info').style.margin='0';
+            document.getElementById('instrucao').style.margin='0';
+            instrucao        
+            
+            //loop para realizar a descriptografia
+            do {
+                let letraA = /ai/i;
+                let novaFraseA = (frase.replace(letraA, 'a'));
+                frase = novaFraseA;
+            } while (frase.indexOf("ai") !== -1);
+        
+            do {
+                let letraE = /enter/i;
+                let novaFraseE = (frase.replace(letraE, 'e'));
+                frase = novaFraseE;
+            } while (frase.indexOf("enter") !== -1);
+
+            do {
+                let letraI = /imes/i;
+                let novaFraseI = (frase.replace(letraI, 'i'));
+                frase = novaFraseI;
+            } while (frase.indexOf("imes") !== -1);
+
+            do {
+                let letraO = /ober/i;
+                let novaFraseO = (frase.replace(letraO, 'o'));
+                frase = novaFraseO;
+            } while (frase.indexOf("ober") !== -1);    
+
+            do {
+                let letraU = /ufat/i;
+                let novaFraseU = (frase.replace(letraU, 'u'));
+                frase = novaFraseU;
+                let novaFrase = frase;
+            } while (frase.indexOf("ufat") !== -1);
+            
+            //tamano do textarea
+            tamanhoTela = window.screen.width;
+            if (tamanhoTela <= 1100){
+                let textArearows = 8; 
+            } else if (tamanhoTela <= 450){
+                let textArearows = 4; 
+            } else{
+                let textArearows = 15; 
+            }
+
+
+            //exibir dados
+            let fraseDescriptografada = document.getElementById('MostraCriptoDescripto');
+
+            //ajuste do textarea lado direito
+            if (resolucaoLargura > 1100){
+                fraseDescriptografada.innerHTML = '<textarea minlength="1" style="height: 655px;" maxlength="380" disabled id="resultado">'+frase+'</textarea>';
+            } else if (resolucaoLargura <= 1100){
+                fraseDescriptografada.innerHTML = '<textarea minlength="1" style="height: 300px;" maxlength="380" disabled id="resultado">'+frase+'</textarea>';
+            } else if(resolucaoLargura <= 450){
+                fraseDescriptografada.innerHTML = '<textarea minlength="1" style="height: 300px;" maxlength="200" disabled id="resultado">'+frase+'</textarea>';
+            }
+            AjustaTextarea();
+        }
     } else {
-
-        document.getElementById('descriptografarBotao').style.cursor='auto';
-        document.getElementById("descriptografarBotao").disabled = false;
-
-        //zerar elementos inicais da tela
-        document.getElementById('apresentacao__conteudo__direita__img').style.background='#FFF';
-        document.getElementById('apresentacao__conteudo__direita__img').style.height='1px';
-        let imagemInicialGet = '';
-        imagemInicialGet.innerHTML= '';
-        tituloH4.innerHTML = '';
-        instrucaoInicialGet.innerHTML = '';
-        botaoCopiarGet.innerHTML = botaoCopiar;
-
-        //selecionar o textarea
-        converteLoweGet = document.getElementById('texto').value;
-        frase = converteLoweGet.toLocaleLowerCase();
-
-        //limpar margin section direita 
-        document.getElementById('apresentacao__conteudo__direita__img').style.margin='0';
-        document.getElementById('apresentacao__conteudo__direita__info').style.margin='0';
-        document.getElementById('instrucao').style.margin='0';
-        instrucao        
-        
-        //loop para realizar a descriptografia
-        do {
-            let letraA = /ai/i;
-            let novaFraseA = (frase.replace(letraA, 'a'));
-            frase = novaFraseA;
-        } while (frase.indexOf("ai") !== -1);
-    
-        do {
-            let letraE = /enter/i;
-            let novaFraseE = (frase.replace(letraE, 'e'));
-            frase = novaFraseE;
-        } while (frase.indexOf("enter") !== -1);
-
-        do {
-            let letraI = /imes/i;
-            let novaFraseI = (frase.replace(letraI, 'i'));
-            frase = novaFraseI;
-        } while (frase.indexOf("imes") !== -1);
-
-        do {
-            let letraO = /ober/i;
-            let novaFraseO = (frase.replace(letraO, 'o'));
-            frase = novaFraseO;
-        } while (frase.indexOf("ober") !== -1);    
-
-        do {
-            let letraU = /ufat/i;
-            let novaFraseU = (frase.replace(letraU, 'u'));
-            frase = novaFraseU;
-            let novaFrase = frase;
-        } while (frase.indexOf("ufat") !== -1);
-        
-        //tamano do textarea
-        tamanhoTela = window.screen.width;
-        if (tamanhoTela <= 1100){
-            let textArearows = 8; 
-        } else if (tamanhoTela <= 450){
-            let textArearows = 4; 
-        } else{
-            let textArearows = 15; 
-        }
-
-        //exibir dados
-        let fraseDescriptografada = document.getElementById('MostraCriptoDescripto');
-
-        //ajuste do textarea lado direito
-        if (resolucaoLargura > 1100){
-            fraseDescriptografada.innerHTML = '<textarea minlength="1" style="height: 655px;" maxlength="380" disabled id="resultado">'+frase+'</textarea>';
-        } else if (resolucaoLargura <= 1100){
-            fraseDescriptografada.innerHTML = '<textarea minlength="1" style="height: 300px;" maxlength="380" disabled id="resultado">'+frase+'</textarea>';
-        } else if(resolucaoLargura <= 450){
-            fraseDescriptografada.innerHTML = '<textarea minlength="1" style="height: 300px;" maxlength="200" disabled id="resultado">'+frase+'</textarea>';
-        }
-        AjustaTextarea();
+        document.getElementById('texto').value='';
+        document.getElementById('texto').placeholder='letras minúsculas e sem acento!';
+        setTimeout(function(){
+            location.reload();
+        }, 1000);
     }
+
 
 }
